dc08f22a2eab2de564ae71317a3a862db562acb8
Watabe Orenji <ekiakmarullah.id@gmail.com>
2024-02-08 11:11:23 +0700

    Upload File Laravel 10 Job Finder Dengan Sudah Terpasang Tema, dan Tampilan FrontEnd

---
+++ resources/views/Template/menuBar.blade.php
@@ -0,0 +1,32 @@
+<div class="col-lg-9 col-md-9">
+    <div class="menu-wrapper">
+        <!-- Main-menu -->
+        <div class="main-menu">
+            <nav class="d-none d-lg-block">
+                <ul id="navigation">
+                    <li><a href="{{ asset('TEMPLATE/index.html')}}">Home</a></li>
+                    <li><a href="{{ asset('TEMPLATE/job_listing.html')}}">Find a Jobs </a></li>
+                    <li><a href="{{ asset('TEMPLATE/about.html')}}">About</a></li>
+                    <li><a href="#">Page</a>
+                        <ul class="submenu">
+                            <li><a href="{{ asset('TEMPLATE/blog.html')}}">Blog</a></li>
+                            <li><a href="{{ asset('TEMPLATE/single-blog.html')}}">Blog Details</a></li>
+                            <li><a href="{{ asset('TEMPLATE/elements.html')}}">Elements</a></li>
+                            <li><a href="{{ asset('TEMPLATE/job_details.html')}}">job Details</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{ asset('TEMPLATE/contact.html')}}">Contact</a></li>
+                </ul>
+            </nav>
+        </div>          
+        <!-- Header-btn -->
+        <div class="header-btn d-none f-right d-lg-block">
+            <a href="#" class="btn head-btn1">Register</a>
+            <a href="#" class="btn head-btn2">Login</a>
+        </div>
+    </div>
+</div>
+<!-- Mobile Menu -->
+<div class="col-12">
+    <div class="mobile_menu d-block d-lg-none"></div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontEndController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// BUAT ROUTE UNTUK MENGARAHKAN KE HALAMAN DEPAN WEBSITE
+Route::get('/', [FrontEndController::class, 'index']);
